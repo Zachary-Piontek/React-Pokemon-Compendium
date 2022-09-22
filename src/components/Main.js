c385538623c65e8d534f4d091caf3e56f7e2a1dc
Zachary Piontek <zakpiontek@gmail.com>
2022-09-22 15:52:59 -0700

    implement display component in main, shows data in browser

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,15 @@
+import { usePokemon } from '../hooks/usePokemon.js';
 import './main.css';
+import PokemonDisplay from './PokemonDisplay.js';
 
 export default function Main() {
-    
+  const { pokemon } = usePokemon();
+
+  return (
+    <div>
+      {pokemon.map((poke) => (
+        <PokemonDisplay {...poke} key={poke.id} />
+      ))}
+    </div>
+  );
 }
--- src/components/PokemonDisplay.js
+++ src/components/PokemonDisplay.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './pokemondisplay.css';
 
-export default function PokemonDisplay({ pokemon, id }) {
+export default function PokemonDisplay({ pokemon, id, type_1 }) {
   return (
     <div>
       <h1>{pokemon}</h1>
       <h2>{id}</h2>
+      <h3>{type_1}</h3>
     </div>
   );
 }
